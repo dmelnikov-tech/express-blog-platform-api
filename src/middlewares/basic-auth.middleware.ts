20df383b096f02ccf1d2f028271667f1f25c64fb
Dmitry Melnikov <dmelnikov.tech@gmail.com>
2025-11-15 00:32:11 +0300

    add basic-auth middleware for blogs

---
+++ src/middlewares/basic-auth.middleware.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from "express";
+import { HTTP_STATUSES } from "../constants/http-statuses.js";
+
+export const basicAuthMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const authHeader = req.headers.authorization;
+
+  if (authHeader === "Basic YWRtaW46cXdlcnR5") {
+    return next();
+  }
+
+  res.sendStatus(HTTP_STATUSES.UNAUTHORIZED);
+};
--- src/routes/blogs.routes.ts
+++ src/routes/blogs.routes.ts
@@ -2,6 +2,7 @@ import { Router, Request, Response } from "express";
 import { blogsService } from "../services/blogs.service.js";
 import { HTTP_STATUSES } from "../constants/http-statuses.js";
 import type { BlogResponseDto } from "../types/domain/blog.types.js";
+import { basicAuthMiddleware } from "../middlewares/basic-auth.middleware.js";
 
 const router = Router();
 
@@ -29,7 +30,7 @@ router.get("/:id", async (req: Request, res: Response) => {
   }
 });
 
-router.post("/", async (req: Request, res: Response) => {
+router.post("/", basicAuthMiddleware, async (req: Request, res: Response) => {
   try {
     const { name, description, websiteUrl } = req.body;
     const blog: BlogResponseDto = await blogsService.createBlog({
@@ -43,7 +44,7 @@ router.post("/", async (req: Request, res: Response) => {
   }
 });
 
-router.put("/:id", async (req: Request, res: Response) => {
+router.put("/:id", basicAuthMiddleware, async (req: Request, res: Response) => {
   try {
     const { id } = req.params;
     const { name, description, websiteUrl } = req.body;
@@ -57,25 +58,29 @@ router.put("/:id", async (req: Request, res: Response) => {
       return res.sendStatus(HTTP_STATUSES.NOT_FOUND);
     }
 
-    res.sendStatus(HTTP_STATUSES.NO_CONTENT)
-  } catch (error) {
-    res.sendStatus(HTTP_STATUSES.INTERNAL_SERVER_ERROR);
-  }
-});
-
-router.delete("/:id", async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-    const deleteResult: boolean = await blogsService.deleteBlog(id);
-
-    if (!deleteResult) {
-      return res.sendStatus(HTTP_STATUSES.NOT_FOUND);
-    }
-
     res.sendStatus(HTTP_STATUSES.NO_CONTENT);
   } catch (error) {
     res.sendStatus(HTTP_STATUSES.INTERNAL_SERVER_ERROR);
   }
 });
+
+router.delete(
+  "/:id",
+  basicAuthMiddleware,
+  async (req: Request, res: Response) => {
+    try {
+      const { id } = req.params;
+      const deleteResult: boolean = await blogsService.deleteBlog(id);
+
+      if (!deleteResult) {
+        return res.sendStatus(HTTP_STATUSES.NOT_FOUND);
+      }
+
+      res.sendStatus(HTTP_STATUSES.NO_CONTENT);
+    } catch (error) {
+      res.sendStatus(HTTP_STATUSES.INTERNAL_SERVER_ERROR);
+    }
+  }
+);
 
 export default router;
